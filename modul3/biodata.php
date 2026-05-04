be65d0a07c042e3f027c0ee3ebe20914e1e33a76
ichul <ikizaki01@gmail.com>
2026-05-04 23:22:41 +0700

    update

---
+++ modul3/biodata.php
@@ -0,0 +1,10 @@
+<?php
+$nim = '23650048';
+$nama = 'Hanson Philip';
+$alamat = 'Jl. Gajah Mada No.4';
+$nilai = 80;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Alamat : ", $alamat, "<br>";
+echo "Nilai : $nilai";
+?>
